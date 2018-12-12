a096953cf0eba579ded0cae2345c15331ae538e8
grumbach <agrumbac@student.42.fr>
2018-12-12 05:02:46 +0000

    [FIX] in_cksum

--- srcs/in_cksum.c
+++ srcs/in_cksum.c
@@ -12,30 +12,37 @@
 
 #include "ft_ping.h"
 
-//TODO fix checksum!
-
 uint16_t		in_cksum(__unused const void *buffer, __unused size_t size)
 {
 	asm volatile (".intel_syntax;\n"
-	"  xor rax, rax\n"
-	"  cmp rsi, 0x1\n"
-	"  jne _loop\n"
-	"_odd_case:\n"
-	"  xor dx, dx\n"
-	"  mov dh , BYTE PTR [rdi]\n"
-	"  sub rsi, 1\n"
-	"  jmp _add_cksum\n"
-	"_loop:\n"
-	"  mov dx, WORD PTR [rdi]\n"
-	"  add rdi, 2\n"
-	"  sub rsi, 2\n"
-	"_add_cksum:\n"
-	"  adc ax, dx\n" //TODO is carry swiched on 16bit overflows?
-	"  cmp rsi, 1\n"
-	"  jg _loop\n"
-	"  je _odd_case\n"
-	"  leave\n"
-	"  ret\n");
+			"  xor rax, rax\n"
+			"  cmp rsi, 0x1\n"
+			"  jne _loop\n"
+			"_odd_case:\n"
+			"  xor rdx, rdx\n"
+			"  mov dl , BYTE PTR [rdi]\n"
+			"  sub rsi, 1\n"
+			"  jmp _add_cksum\n"
+			"_loop:\n"
+			"  xor rdx, rdx\n"
+			"  mov dx, WORD PTR [rdi]\n"
+			"  add rdi, 2\n"
+			"  sub rsi, 2\n"
+			"_add_cksum:\n"
+			"  add rax, rdx\n"
+			"  cmp rsi, 1\n"
+			"  jg _loop\n"
+			"  je _odd_case\n"
+			"  mov rcx, rax\n"
+			"  shr rcx, 16\n"
+			"  and rax, 0xffff\n"
+			"  add rax, rcx\n"
+			"  mov rcx, rax\n"
+			"  shr rcx, 16\n"
+			"  add rax, rcx\n"
+			"  not rax\n"
+			"  leave\n"
+			"  ret\n");
 
 	__builtin_unreachable();
 }
